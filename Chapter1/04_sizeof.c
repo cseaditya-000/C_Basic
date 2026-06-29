99d6e0b4a3d9fd4ab84847fea2e27a06d3589b06
cseaditya-000 <aditya3546raj@gmail.com>
2026-06-29 13:44:45 +0530

    Initial commit

---
+++ Chapter1/04_sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("char = %zu bytes\n", sizeof(char));
+    printf("int = %zu bytes\n", sizeof(int));
+    printf("float = %zu bytes\n", sizeof(float));
+    return 0;
+}
