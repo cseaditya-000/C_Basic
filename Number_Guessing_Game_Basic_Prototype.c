3ccc7c3a833e3bed3d7cc876e4060cdb1e1e6399
cseaditya-000 <aditya3546raj@gmail.com>
2026-06-30 10:53:57 +0530

    I was just trying. Don't laugh.

---
+++ Number_Guessing_Game_Basic_Prototype.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0;
+    int d = 45;
+    while (a != d)
+    {
+        printf("Guess the number: ");
+        scanf("%d",&a);
+        if ((d-a) >= 10)
+        {
+            printf("Too less\n");
+        }
+        else if(a < d)
+        {
+            printf("Move a bit high.\n");
+        }
+        else if((a - d) >= 10)
+        {
+            printf("Too high\n");
+        }
+        else if(a > d)
+        {
+            printf("Move a bit less.\n");
+        }
+    }
+    printf("Guessed Correectly.\n");
+    return 0;
+}
