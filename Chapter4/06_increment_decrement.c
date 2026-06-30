c1315a50eed4ab345a21f465b58c0a95a11c76fe
cseaditya-000 <aditya3546raj@gmail.com>
2026-06-30 09:26:54 +0530

    -

--- Chapter4/06_increment_decrement.c
+++ Chapter4/06_increment_decrement.c
@@ -9,8 +9,16 @@ int main(){
     printf("The value of i is %d\n",i);
     ++i;//i=i+1
     printf("The value of i is %d\n",i);
-    i+=4;//i=i+4
+    i+=4;//i=i+4  //  *=  /=  -=  +=
     printf("The value of i is %d\n",i);
-    printf()
+    printf("The value of i is %d\n",i++);//i++ first prints then increments.(Post increment operator)
+    printf("The value of i id %d\n",i);
+    printf("The value of i is %d\n",++i);//++i first increments then prints.(Pre increment operator)
+    i--;//i=i-1
+    printf("The value of i is %d\n",i--);
+    printf("The value of i is %d\n",i);
+    printf("The value of i is %d\n",--i);
+    int age=18;
+    printf("%p",&ageg);
     return 0;
 }
