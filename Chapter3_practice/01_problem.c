300ab023448fac4a0c454805af586a2e77a77d3f
cseaditya-000 <aditya3546raj@gmail.com>
2026-06-30 07:49:15 +0530

    -

--- Chapter3_practice/01_problem.c
+++ Chapter3_practice/01_problem.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-//The answer is 'I am 11'
+//The answer is 'I am 11' bacuse a=11 is assignment operator
 int main(){
-    int a = 10; 
-    if (a == 11) {printf("I am 11");} 
-    else  {printf("I am not 11");}
+    int a = 11; 
+    if (a == 11) printf("I am 11"); //Also a way to write if else
+    else  printf("I am not 11");
     return 0;
 }
---
+++ Chapter4/06_increment_decrement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int i=5;
+    printf("The value of i is %d\n",i);
+    i++;//i=i+1
+    printf("The value of i is %d\n",i);
+    i=i+5;
+    printf("The value of i is %d\n",i);
+    ++i;//i=i+1
+    printf("The value of i is %d\n",i);
+    i+=4;//i=i+4
+    printf("The value of i is %d\n",i);
+    printf()
+    return 0;
+}
